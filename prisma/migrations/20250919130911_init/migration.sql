e7d1a042f3d265a62dbfa04fce85bac903c1725e
n2 <n2@Naveens-MacBook-Pro.local>
2025-09-19 23:54:02 +0530

    postgresql-setup-done

---
+++ prisma/migrations/20250919130911_init/migration.sql
@@ -0,0 +1,44 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT,
+    "privyId" TEXT,
+    "walletAddr" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Order" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "walletAddr" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'PENDING',
+    "utrNumber" VARCHAR(50),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "completedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_privyId_key" ON "public"."User"("privyId");
+
+-- CreateIndex
+CREATE INDEX "Order_userId_idx" ON "public"."Order"("userId");
+
+-- CreateIndex
+CREATE INDEX "Order_status_idx" ON "public"."Order"("status");
+
+-- CreateIndex
+CREATE INDEX "Order_utrNumber_idx" ON "public"."Order"("utrNumber");
+
+-- AddForeignKey
+ALTER TABLE "public"."Order" ADD CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
